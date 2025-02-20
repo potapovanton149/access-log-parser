af50419d25df8936788a96aadd736612f3b59a2c
Potapov Anton <exchialle@gmail.com>
2025-02-20 22:48:30 +0400

    Изменена логика класса main в соответствии с заданием из темы №3.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+
+        int a, b;
+
+        System.out.println("Введите первое число:");
+        a = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        b = new Scanner(System.in).nextInt();
+
+        System.out.println(
+                "Сумма чисел: " + (a + b) + "\n"
+                        + "Разность чисел: " + (a - b) + "\n"
+                        + "Произведение чисел: " + (a * b) + "\n"
+                        + "Частное чисел: " + (a / b) + "\n"
+        );
     }
 }
