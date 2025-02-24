d94c51608658bb17e72e77fcf26a9013921a842d
Potapov Anton <exchialle@gmail.com>
2025-02-24 21:55:55 +0400

    Изменена логика класса main в соответствии с заданием курсового проекта темы №5.Циклы.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " +
-                Math.random());
+        int numberOfFiles = 0;
+
+        while (true) {
+            System.out.println("Введите полный путь к файлу:");
+
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                System.out.println("Указанный файл не существует или указанный путь является путём к папке");
+            } else {
+                numberOfFiles++;
+                System.out.print("Путь к файлу указан верно. Это файл номер №" + numberOfFiles + ".");
+            }
+        }
     }
 }
