9e02eec8bfed458d9e8f9317c62c9a496805a699
Potapov Anton <exchialle@gmail.com>
2025-04-25 22:27:25 +0400

    Учтены и исправлены замечания Влада.

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,21 +1,27 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.zip.ZipOutputStream;
 
 public class Statistics {
-    private long totalTraffic;
-    private LocalDateTime minTime;
-    private LocalDateTime maxTime;
-    private HashSet<String> sitePages;
-    private HashMap<String, Integer> countsOS;
-    private HashMap<String, Double> statsOS;
+    private static final Logger log = LoggerFactory.getLogger(Statistics.class);
+    private long totalTraffic; //общая сумма трафика данных
+    private LocalDateTime minTime; //минимальное время из лога
+    private LocalDateTime maxTime; //максимальное время из лога
+    private final HashSet<String> sitePages; //список всех страниц из лога
+    private final HashMap<String, Integer> infoCountsOS; //статистика по ОС и количеству
+    private final HashMap<String, Double> infoStatsOS; //статистика по ОС и значение долей
 
+    public HashMap<String, Integer> getInfoCountsOS() {
+        return infoCountsOS;
+    }
 
-    public HashMap<String, Integer> getCountsOS() {
-        return countsOS;
+    public HashMap<String, Double> getInfoStatsOS() {
+        return infoStatsOS;
     }
 
     public HashSet<String> getSitePages() {
@@ -27,13 +33,15 @@ public class Statistics {
         this.maxTime = LocalDateTime.MIN;
         this.minTime = LocalDateTime.MAX;
         this.sitePages = new HashSet<>();
-        this.countsOS = new HashMap<>();
-        this.statsOS = new HashMap<>();
+        this.infoCountsOS = new HashMap<>();
+        this.infoStatsOS = new HashMap<>();
     }
 
     public void addEntry(LogEntry logEntry) {
+        //считаем объем каждого запроса и суммируем
         totalTraffic += logEntry.getSizeDate();
 
+        //указываем минимальное и максимальное время запроса в логе
         if (logEntry.getDataTime().isBefore(minTime)) {
             minTime = logEntry.getDataTime();
         }
@@ -41,45 +49,40 @@ public class Statistics {
             maxTime = logEntry.getDataTime();
         }
 
+        //добавляем в лист путь запроса если статус 200
         if (logEntry.getHttpCode().equals("200")) {
             sitePages.add(logEntry.getPathRequest());
         }
 
+        //берем из user agent ОС и если значение не пустое +1 к мапе ОС
         String os = logEntry.getUserAgent().getOs();
-
-        if (!os.isEmpty()) {
-            if (!countsOS.containsKey(os)) {
-                countsOS.put(os, 1);
+        if (os.isEmpty()) {
+            if (!infoCountsOS.containsKey("empty")) {
+                infoCountsOS.put("empty", 1);
             } else {
-                countsOS.put(os, countsOS.get(os) + 1);
+                infoCountsOS.put("empty", infoCountsOS.get("empty") + 1);
             }
+        } else if (!infoCountsOS.containsKey(os)) {
+            infoCountsOS.put(os, 1);
+        } else {
+            infoCountsOS.put(os, infoCountsOS.get(os) + 1);
         }
+
+        //расчет доли от общего значения числа по каждой ОС
         int totalCountOS = 0;
-        for (Map.Entry<String, Integer> entry : countsOS.entrySet()) {
+        for (Map.Entry<String, Integer> entry : infoCountsOS.entrySet()) {
             totalCountOS += entry.getValue();
         }
-
-        for (Map.Entry<String, Integer> entry : countsOS.entrySet()) {
+        for (Map.Entry<String, Integer> entry : infoCountsOS.entrySet()) {
             double d = (double) entry.getValue() / totalCountOS;
-            statsOS.put(entry.getKey(), d);
+            infoStatsOS.put(entry.getKey(), Math.round(d * 1000.0) / 1000.0);
         }
     }
 
+    //подсчет среднего трафика за час
     public double getTrafficRate() {
         long hours = Duration.between(minTime, maxTime).toHours();
         hours = Math.max(1, hours);
         return (double) totalTraffic / hours;
-    }
-
-    public HashMap<String, Double> getStatsOS() {
-        int totalRequests = 0;
-        for (int count : countsOS.values()) {
-            totalRequests += count;
-        }
-        for (String os : countsOS.keySet()) {
-            double value= (double) countsOS.get(os) / totalRequests;
-            statsOS.put(os, Math.round(value * 1000.0) / 1000.0);
-        }
-        return statsOS;
     }
 }
